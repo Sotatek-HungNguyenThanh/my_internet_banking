03fd47247c4bdbfdf9a5e35b099a9c720bbfbf21
hnsotatek <hung.nguyen.thanh@sotatek.com>
2017-03-28 11:26:44 +0700

    create layout login

---
+++ src/main/java/com/beepig/Controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.beepig.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "")
+    public String guest() {
+        return "guest";
+    }
+}
